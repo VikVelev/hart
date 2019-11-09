04aa8a0cc336d2d69ca0082dcf027835cd729c45
Lyuben Todorov <lyuben.todorov@gmail.com>
2019-11-09 19:48:43 +0100

    form done no animations

---
+++ frontend/src/Components/Map.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { observer } from 'mobx-react-lite'
+
+@observer
+class Map extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Map
--- frontend/src/Components/Profile.js
+++ frontend/src/Components/Profile.js
@@ -16,61 +16,66 @@ class Profile extends Component {
             display: 4 // max sites on page 
         };
         this.onDecision = this.onDecision.bind(this);
-        this.takeRandom = this.takeRandom.bind(this);3
+        this.takeRandom = this.takeRandom.bind(this); 3
     }
     componentDidMount() {
-        const sites = [{ key: "a", name: "Kur 1", picked: false }, 
-        { key: "b", name: "Kur 2", picked: false }, 
-        { key: "c", name: "Kur 3", picked: false }, 
-        { key: "d", name: "Kur 4", picked: false }, 
-        { key: "e", name: "Kur 5", picked: false }, 
-        { key: "f", name: "Kur 6", picked: false }, 
+        const sites = [{ key: "a", name: "Kur 1", picked: false },
+        { key: "b", name: "Kur 2", picked: false },
+        { key: "c", name: "Kur 3", picked: false },
+        { key: "d", name: "Kur 4", picked: false },
+        { key: "e", name: "Kur 5", picked: false },
+        { key: "f", name: "Kur 6", picked: false },
         { key: "g", name: "Kur 8", picked: false },
         { key: "h", name: "Kur 9", picked: false },
         { key: "i", name: "Kur 10", picked: false }]
+
+
+
         const shuffle = this.takeRandom(this.state.display, sites.length); // take display amount of indexes from site.length indexes
-        const sitesOnDisplay = sites.filter((site,index) => shuffle.includes(index)); // filter sites with matching index into onDisplay state 
-        sites.forEach((site,index)=> {
-            if(shuffle.includes(index)){
+        const sitesOnDisplay = sites.filter((site, index) => shuffle.includes(index)); // filter sites with matching index into onDisplay state 
+        sites.forEach((site, index) => {
+            if (shuffle.includes(index)) {
                 site.picked = true;
             }
         })
-        this.setState({...this.state, sites: sites, sitesOnDisplay: sitesOnDisplay});
+        this.setState({ ...this.state, sites: sites, sitesOnDisplay: sitesOnDisplay });
     }
 
 
-    takeRandom(displaySize, arraySize){
+    takeRandom(displaySize, arraySize) {
         let arr = [];
-        while(arr.length<displaySize){
-            let r = Math.floor(Math.random()*arraySize)
-            if(arr.indexOf(r) === -1) arr.push(r);
+        while (arr.length < displaySize) {
+            let r = Math.floor(Math.random() * arraySize)
+            if (arr.indexOf(r) === -1) arr.push(r);
         }
         return arr;
     }
 
     //returns index of possible element
-    newRandom(array){
+    newRandom(array) {
         //filter unpicked sites
         const choices = array.filter(site => site.picked === false);
         //return random unpicked site
-        return choices[Math.floor(Math.random()*choices.length)]
+        return choices[Math.floor(Math.random() * choices.length)]
     }
     onDecision(key, accepted) {
         let sites = this.state.sites;
-        let sitesOnDisplay  = this.state.sitesOnDisplay;
+        let sitesOnDisplay = this.state.sitesOnDisplay;
 
         const replacement = this.newRandom(sites); // get replacement from existing state
-        sites.forEach((site,index) => {
-            if(site.key == replacement.key){
+        sites.forEach((site, index) => {
+            if (site.key == replacement.key) {
                 site.picked = true;
             }
         })
-        sitesOnDisplay = sitesOnDisplay.map((value,index)=> value.key === key ? replacement : value) //remove accepted key 
+        sitesOnDisplay = sitesOnDisplay.map((value, index) => value.key === key ? replacement : value) //remove accepted key 
 
         this.props.store.addSite(key, accepted) // updates store      
-        this.setState({...this.state, sites:sites, 
-            sitesOnDisplay:sitesOnDisplay, 
-            progress: this.state.progress >= 100 ? 100 : this.state.progress + 20  }) // push state changes
+        this.setState({
+            ...this.state, sites: sites,
+            sitesOnDisplay: sitesOnDisplay,
+            progress: this.state.progress >= 100 ? 100 : this.state.progress + 20
+        }) // push state changes
 
     }
 
@@ -87,6 +92,7 @@ class Profile extends Component {
 
                     </Card.Group>
                     <Progress className="progressBar" percent={this.state.progress} indicating />
+                    {this.state.submitted ? <Redirect push to="/map"></Redirect> : null}
 
                 </Container>
             </div>
