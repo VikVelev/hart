6761c60594e39e99521858249981e73b88e6cd7d
Viktor Velev <viktorvelev8@gmail.com>
2019-11-10 00:16:16 +0100

    Welcome screen working, some bugfixes, responsiveness

--- frontend/src/Components/Map.js
+++ frontend/src/Components/Map.js
@@ -6,20 +6,16 @@ class Map extends Component {
     render() {
         return (
             <LoadScript
-                id="script-loader"
-                googleMapsApiKey="AIzaSyB6nG0vhkO7C_97rSGJhs2cYm3rKE7bUf8"
-                googleMapsClientId="">
+                    id="script-loader"
+                    googleMapsApiKey="AIzaSyB6nG0vhkO7C_97rSGJhs2cYm3rKE7bUf8"
+                    googleMapsClientId="">
                 <GoogleMap
                     id="circle-example"
                     mapContainerStyle={{
                         height: "100%",
                         width: ""
                     }}
-                    zoom={11}center={{lat:52.3366039, lng:4.8667033}}/>
-
-
-
-
+                    zoom={11} center={{lat:52.3366039, lng:4.8667033}}/>
             </LoadScript >
         )
     }
