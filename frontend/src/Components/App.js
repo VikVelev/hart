173b19f2970ec6370650610b4d7ed9d38359348a
Lyuben Todorov <lyuben.todorov@gmail.com>
2019-11-09 21:51:09 +0100

    maps

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -4,17 +4,24 @@ import Store from '../Store';
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import WelcomeScreen from './WelcomeScreen';
 import Profile from './Profile';
-
+import Map from './Map';
+import Directions from './Directions';
 function App() {
     return (
         <BrowserRouter>
-            <div className = "App">
+            <div className="App">
                 <Switch>
                     <Route path="/welcome">
                         <WelcomeScreen />
                     </Route>
                     <Route path="/profile">
-                        <Profile store = {Store} />
+                        <Profile store={Store} />
+                    </Route>
+                    <Route path="/map">
+                        <Map></Map>
+                    </Route>
+                    <Route path="/kur">
+                            <Directions />
                     </Route>
                 </Switch>
             </div>
--- frontend/src/Components/Map.js
+++ frontend/src/Components/Map.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
-import { observer } from 'mobx-react-lite'
-
+import { observer } from 'mobx-react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
 @observer
 class Map extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <LoadScript
+                id="script-loader"
+                googleMapsApiKey="AIzaSyB6nG0vhkO7C_97rSGJhs2cYm3rKE7bUf8"
+                googleMapsClientId="">
+                <GoogleMap
+                    id="circle-example"
+                    mapContainerStyle={{
+                        height: "100%",
+                        width: ""
+                    }}
+                    zoom={11}center={{lat:52.3366039, lng:4.8667033}}/>
+
+
+
+
+            </LoadScript >
         )
     }
 }
