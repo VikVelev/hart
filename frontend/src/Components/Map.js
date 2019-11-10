39b736bde95da141a73c92880026b932fa87a200
Viktor Velev <viktorvelev8@gmail.com>
2019-11-10 03:48:18 +0100

    Some animations and refactoring

--- frontend/src/Components/Map.js
+++ frontend/src/Components/Map.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
+import {Transition} from 'semantic-ui-react'
 import { GoogleMap, LoadScript } from '@react-google-maps/api'
+
 @observer
 class Map extends Component {
     render() {
         return (
-            <LoadScript
-                    id="script-loader"
-                    googleMapsApiKey="AIzaSyB6nG0vhkO7C_97rSGJhs2cYm3rKE7bUf8"
-                    googleMapsClientId="">
-                <GoogleMap
-                    id="circle-example"
-                    mapContainerStyle={{
-                        height: "100%",
-                        width: ""
-                    }}
-                    zoom={11} center={{lat:52.3366039, lng:4.8667033}}/>
-            </LoadScript >
+            <Transition transitionOnMount animation="scale" duration={500}>
+                <LoadScript
+                        id="script-loader"
+                        googleMapsApiKey="AIzaSyB6nG0vhkO7C_97rSGJhs2cYm3rKE7bUf8"
+                        googleMapsClientId="">
+                    <GoogleMap
+                        id="circle-example"
+                        mapContainerStyle={{
+                            height: "100%",
+                            width: ""
+                        }}
+                        zoom={11} center={{lat:52.3366039, lng:4.8667033}}/>
+                </LoadScript >
+            </Transition>
         )
     }
 }
